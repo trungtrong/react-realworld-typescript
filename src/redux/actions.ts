52e6de39c67a1f322a52ca21dfdc31c10a153f75
Trong Ngo <trong.ngotrung@gmail.com>
2023-10-09 15:52:39 +0700

    feat(Redux Toolkit): set up redux-toolkit

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,7 +1,7 @@
 import { ADD_TODO, SET_FILTER, TOGGLE_TODO } from "./actionTypes";
 
 let nextTodoId = 0;
-
+// Actions
 export const addTodo = (content: string) => ({
     type: ADD_TODO,
     payload: {
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,11 @@
-import { createStore } from "redux";
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+// export default configureStore(rootReducer);
+const store = configureStore({
+    reducer: rootReducer
+})
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
